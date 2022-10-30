012062634b4e32f2cdac5626072ad43de937407e
evanlin96069 <72735402+evanlin96069@users.noreply.github.com>
2022-10-30 19:31:28 -0400

    Added debug.

--- compile.sh
+++ compile.sh
@@ -3,6 +3,12 @@
 CC=gcc
 
 warnings="-Wall -Wextra -pedantic -std=c99"
-cflags="-Os"
+
+dbg=0
+if [ $dbg = 1 ]; then
+    cflags="-g3"
+else
+    cflags="-Os -s"
+fi
 
 $CC src/*.c -o nino $warngins $cflags
