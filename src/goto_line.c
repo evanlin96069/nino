62c56f038825094eadd6b650ea99d9c7169fe74f
evanlin96069 <72735402+evanlin96069@users.noreply.github.com>
2023-02-07 16:57:28 -0500

    Add error message for goto line

--- src/goto_line.c
+++ src/goto_line.c
@@ -4,6 +4,7 @@
 #include "editor.h"
 #include "input.h"
 #include "output.h"
+#include "status.h"
 
 void editorGotoLine() {
     char* query = editorPrompt("Goto line: %s", GOTO_LINE_MODE, NULL);
@@ -13,12 +14,16 @@ void editorGotoLine() {
     if (line < 0) {
         line = E.num_rows + 1 + line;
     }
+
     if (line > 0 && line <= E.num_rows) {
         E.cursor.x = 0;
         E.sx = 0;
         E.cursor.y = line - 1;
         editorScroll();
+    } else {
+        editorSetStatusMsg("Type a line number between 1 to %d.", E.num_rows);
     }
+
     if (query) {
         free(query);
     }
